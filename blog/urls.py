13e773211569a4343c1d8e48a122ad2222e074d7
PankajLal99 <lalpankaj0@gmail.com>
2020-03-21 15:06:37 +0530

    models added

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('post/<str:pk>/',views.show,name='show'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    data=Post.objects.filter(status=1).order_by('-created_on')
+    context={
+        'data':data
+    }
+    return render(request,'index.html',context)
+
+def show(request,pk):
+    content=Post.objects.filter(id=pk)
+    context={
+        'content':content
+    }
+    return render(request,'show.html',context)
